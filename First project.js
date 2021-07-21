22859e2cc805c667d317190e4d5325d6b3390939
Kaito Sugimura <Sugimurakaito@gmail.com>
2021-07-20 22:47:04 -0600

    Finish simple randomizer (fortune teller?)

---
+++ First project.js
@@ -0,0 +1,37 @@
+Fortunes = ['Daikichi', 'Chukichi', 'Shokichi', 'kyo', 'daikyo']
+Colors = ['Yellow', 'Green', 'Blue', 'Purple', 'Red', 'Orange', 'Pink', 'White', 'Black']
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//Make your own fortune or thing you want to randomize here
+//Note you can add as how many you wish
+MAKEYOUROWNFORTUNE = ['ADDHERE']
+
+const Fort = {
+    TodaysFortune(name, array) {
+        index = Math.floor(Math.random()*array.length)
+        Fort[name] = array[index]
+    },
+    print(){
+        for (const Fo in Fort){
+            if (typeof(Fort[Fo]) != 'function') {
+                console.log(`Your ${Fo} is ${Fort[Fo]}`)
+            }
+        }
+    }
+}
+
+function CF(name, ar){
+    Fort.TodaysFortune(name, ar)
+}
+
+function print(){
+    Fort.print()
+}
+
+
+CF('Fortune', Fortunes)
+CF('Color', Colors)
+CF('Number', number)
+//Update your fortune name here
+CF('Make your own here', MAKEYOUROWNFORTUNE)
+print() //This function simply prints all 
+
